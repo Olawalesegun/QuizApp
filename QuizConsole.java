b9c7d299cba9d93f93e112fbecda5f893304bd05
Olawalesegun <olawalesegun0@gmail.com>
2023-08-08 23:44:08 +0100

    Quiz Console CLass, carries game Logic

--- QuizConsole.java
+++ QuizConsole.java
@@ -1,21 +1,93 @@
 import java.util.Scanner;
 
 public class QuizConsole {
-    private Teacher teacherQuestion = new Teacher();
-    private Student studentResponse = new Student();
+
+    Teacher teacher = new Teacher();
+    Student student = new Student();
+    Scanner scanInput = new Scanner(System.in);
+    private String studentResponse;
+    private int teacherInput;
+    private int studentScore;
+
+    public void startQuiz(){
+        System.out.println(
+                """
+                    =================================================
+                                WELCOME TO THE QUIZ APP
+                    ================================================  
+                    Hi Student, Kindly reply with your name.
+                """
+        );
+        takeStudentName();
+        System.out.println("""
+                       =========================================
+                              THE GAME WILL NOW COMMENCE
+                          ==================================
+                """);
+
+        System.out.println("""
+                As the teacher, kindly Press your preferred number
+                    from 1 - 3 to pick the question for student. 
+                """);
+        teacherInput = takeTeacherInput();
+        teacherSetsQuestion(teacherInput);
+
+        System.out.println("""
+                Hi Student, Kindly enter your response
+                """);
+        studentResponse = studentResponseInput();
+    }
+    public void validateStudentResponse(String input){
+        if(input.equals(teacher.getAnswers(teacherInput))){
+            studentScore++;
+        }
+        else{
+            studentScore--;
+        }
+    }
+    public void teacherSetsQuestion(int intake){
+        teacher.getQuestion(intake);
+    }
+    public int takeTeacherInput(){
+        int teacherInputToStartQuizForStudent = scanInput.nextInt();
+        return teacherInputToStartQuizForStudent;
+    }
+    public void takeStudentName(){
+        System.out.println("Hi, Student!! Enter your name");
+        String studentNameIntake = studentResponseInput();
+        student.setName(studentNameIntake);
+    }
+    public String getStudentName(){
+        return student.getStudentName();
+    }
+    public String studentResponseInput(){
+        String studentResponse = scanInput.nextLine();
+        return studentResponse;
+    }
+   /* public void setQuiz(){
+        teacher .getQuestion();
+    }
+*/
+
+ /*   private Teacher teacherQuestion = new Teacher();
+    private Student studentResponse = new Student();*//*
     Scanner scan = new Scanner(System.in);
 
     Teacher[] teacherQuestions = new Teacher[5];
     Student[] studentResponses = new Student[5];
 
+    public QuizConsole(){
+        teacherQuestions[0] = new Teacher
+    }
 
     public void setQuiz(){
         int count = 0;
-        String question = null;
+     *//*   String question = null;
         String option1 = null;
         String option2 = null;
         String option3 = null;
-        String option4 = null;
+        String option4 = null;*//*
+
         for(Teacher teacherQ: teacherQuestions){
             System.out.println("Kindly Enter the ID");
             int id = count + 1;
@@ -38,14 +110,14 @@ public class QuizConsole {
         for(Teacher teacherQ: teacherQuestions){
             for(Student studentResp: studentResponses){
                 teacherQ.getQuestion();
-                studentResp.
-                studentResponses.get
+                studentResp.getResponse();
+                studentResponses
             }
 
         }
     }
     public void setStudentDetails(){
         System.out.println("What's your name?");
-    }
+    }*/
 
 }
